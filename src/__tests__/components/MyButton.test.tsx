9d88a4f095470e8d25c35bf9cd5f02554cd10028
Mutuba <danielmutubait@gmail.com>
2024-07-08 15:28:32 +0300

    Test hook example

--- src/__tests__/components/MyButton.test.tsx
+++ src/__tests__/components/MyButton.test.tsx
@@ -1,6 +1,5 @@
 import { screen, render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-// import "@testing-library/jest-dom";
 import { MyButton } from "../../components/MyButton";
 
 describe("MyButton", () => {
